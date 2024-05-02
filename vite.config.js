2edd0652b4b4b1863e701baec234902edacb1228
sergimasl <maslinikovsergey@gamil.com>
2024-05-02 14:05:14 -0400

    depl

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/"
+  base: "/coffee_house-react-sass/"
 })
